01a85190cfbb1a0800ef6142e6a6d0e7a39838c9
luanferreira2 <luan.silva115@fatec.sp.gov.br>
2024-11-11 17:01:36 -0300

    match-case

---
+++ Exemplo_8.1.py
@@ -0,0 +1,18 @@
+n = -1
+
+while n != 0:
+
+    n = int(input('Digite um inteiro: '))
+    
+    match n: 
+        case 1 :
+            print('     você digitou um')
+        case 2:
+            print('     você digitou dois')
+        case 3:
+            print('     voce digitou três')
+        case _:
+            print('     voce digitou outra coisa')
+
+
+print(' Fim do Programa')
